7010ad3f029629fb8678a141d3b750700dfdee3f
MadhavaSudharsan <153193881+MadhavaSudharsan@users.noreply.github.com>
2025-06-30 17:03:04 +0530

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import { HTML5Backend } from 'react-dnd-html5-backend';
-import { DndProvider } from 'react-dnd';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { makeServer } from './api/server';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { HTML5Backend } from "react-dnd-html5-backend";
+import { DndProvider } from "react-dnd";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { makeServer } from "./api/server";
 
-// Start MirageJS server only in development env
-if (process.env.NODE_ENV === 'development') {
-  makeServer();
-}
+makeServer();
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+	document.getElementById("root") as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <DndProvider backend={HTML5Backend}>
-      <App />
-    </DndProvider>
-  </React.StrictMode>
+	<React.StrictMode>
+		<DndProvider backend={HTML5Backend}>
+			<App />
+		</DndProvider>
+	</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
